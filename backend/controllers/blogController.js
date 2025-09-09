d41a2d6cf70dec53f8f26c5d74364cddf75a072b
Sameer <iyazshaikh2276@gmail.com>
2025-09-09 21:05:23 +0530

    fix bug of topics

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -4,30 +4,30 @@ import ErrorHandler from "../utils/errorHandler.js";
 
 // pagination
 export const paginatedBlogs = TryCatch(
-    async(req,res,next)=>{
+  async (req, res, next) => {
 
-        let category = req.query.category
-        let page = parseInt(req.query.page) || 1;
-        if (page < 1) page = 1;
-        if(!category){
-          return next(new ErrorHandler("Category is required!", 400))
-        }
-        const limit = 10
-        const start = (page-1) * limit
-        
-        const blogs = await Blog.find({authorId:req.user._id, category:category}).skip(start).limit(limit).sort({createdAt:-1}).lean()
-
-        const totalBlogs = await Blog.countDocuments({ authorId: req.user._id, category });
-
-        const numPages = Math.ceil(totalBlogs / limit)
-        
-        return res.status(200).json({
-            success:true,
-            message:"Blogs fetched successfully!",
-            data:blogs,
-            pages:numPages
-        })
+    let category = req.query.category
+    let page = parseInt(req.query.page) || 1;
+    if (page < 1) page = 1;
+    if (!category) {
+      return next(new ErrorHandler("Category is required!", 400))
     }
+    const limit = 10
+    const start = (page - 1) * limit
+
+    const blogs = await Blog.find({ authorId: req.user._id, category: category }).skip(start).limit(limit).sort({ createdAt: -1 }).lean()
+
+    const totalBlogs = await Blog.countDocuments({ authorId: req.user._id, category });
+
+    const numPages = Math.ceil(totalBlogs / limit)
+
+    return res.status(200).json({
+      success: true,
+      message: "Blogs fetched successfully!",
+      data: blogs,
+      pages: numPages
+    })
+  }
 )
 
 
@@ -127,35 +127,34 @@ export const deleteBlog = TryCatch(async (req, res, next) => {
 
 // get all topics
 export const getTopicsByCategory = TryCatch(
-    async(req,res,next)=>{
-        const {id} = req.params
-        
-        const blog = await Blog.findById(id)
-        if(!blog){
-            return next(new ErrorHandler("Blog not found!", 404))
-        }
+  async (req, res, next) => {
+    const { id } = req.params
 
-        const topics = await Blog.find({authorId:req.user._id, category:blog.category}).sort({ createdAt:1}).select("_id topic").lean()
-``
-        return res.status(200).json({
-            success:true,
-            message:"Blogs fetched successfully!",
-            data:topics
-        })
-        
+    const blog = await Blog.findById(id)
+    if (!blog) {
+      return next(new ErrorHandler("Blog not found!", 404))
     }
+
+    const topics = await Blog.find({ authorId: req.user._id, category: blog.category }).sort({ createdAt: 1 }).select("_id topic").lean()
+    return res.status(200).json({
+      success: true,
+      message: "Blogs fetched successfully!",
+      data: topics
+    })
+
+  }
 )
 
 
 export const getAllCategories = TryCatch(
   async (req, res, next) => {
-      // Get unique categories from Blog collection
-      const categories = await Blog.find({authorId:req.user._id}).distinct("category")
+    // Get unique categories from Blog collection
+    const categories = await Blog.find({ authorId: req.user._id }).distinct("category")
 
-      return res.status(200).json({
-        success: true,
-        categories,
-      });
+    return res.status(200).json({
+      success: true,
+      categories,
+    });
   }
 );
 
